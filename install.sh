adbb8698d235bb35ff27a33700319c545fb16b2d
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-15 01:18:12 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+pkg update -y
+
+pkg upgrade -y
+
+pkg install python -y
+
+pkg install wget -y
+
+pkg install ffmpeg libwebp -y
+
+pkg install nodejs -y
+
+pkg install npm -y
+
+pkg install tesseract -y
+
+pkg install imagemagick -y
+
+npm i node-tesseract-ocr
+
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+
+npm install
+
+npm audit fix
